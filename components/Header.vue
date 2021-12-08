b4c834bd602929a112e889246ea679ab6144205b
appleRui <apple31581@gmail.com>
2021-12-08 21:19:37 +0900

    Feature/211208/Header And Footer

---
+++ components/Header.vue
@@ -0,0 +1,20 @@
+<template>
+  <b-navbar variant="faded" type="light">
+    <div class="container">
+      <b-navbar-brand tag="h1">東京電機大学</b-navbar-brand>
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav class="ml-auto">
+          <b-nav-item to="/">ナビ01</b-nav-item>
+        </b-navbar-nav>
+      </b-collapse>
+    </div>
+  </b-navbar>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+</style>
