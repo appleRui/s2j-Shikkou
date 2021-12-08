4bef487e028429617f4ccc81dd8abefd42ac70ca
appleRui <apple31581@gmail.com>
2021-12-08 21:26:45 +0900

    Add/211208/MV-Component With CLass-Grant

---
+++ components/MainVisual.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="mv">
+    <div class="mv-wrapper">
+        <h2 class="mv-wraper__welcome-txt"></h2>
+        <b-button class="mv-wrapper__contact-btn">
+          お問い合わせフォーム
+        </b-button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
