ebd4f877a2ff453628859145dca442e381fb926c
appleRui <apple31581@gmail.com>
2021-12-16 09:15:14 +0900

    Add/211216/Vee-validate Module

--- nuxt.config.js
+++ nuxt.config.js
@@ -44,7 +44,8 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
-    '~/plugins/notion'
+    '~/plugins/notion',
+    '~/plugins/vee-validate',
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
