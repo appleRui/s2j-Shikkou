7499075ba09ad48980c96b25d5c69f7b55e910c7
appleRui <apple31581@gmail.com>
2021-12-17 14:56:46 +0900

    feature/211217/Fotter Design

---
+++ components/Footer.vue
@@ -0,0 +1,14 @@
+<template>
+  <footer class="d-flex justify-content-center aligin-center footer">
+    <b-container>
+      <p class="footer-txt">東京電機大学 工学部第二部 自治会執行委員会</p>
+    </b-container>
+  </footer>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -2,6 +2,6 @@
   <div id="app">
     <Header />
       <Nuxt />
-    <Fotter />
+    <Footer />
   </div>
 </template>
