1bac8cc5adcff1fc3dbfa7128fd0d9e6a6699fb8
appleRui <apple31581@gmail.com>
2021-12-17 13:11:44 +0900

    delete/211217/ContactForm

--- pages/index.vue
+++ pages/index.vue
@@ -3,34 +3,12 @@
     <MainVisual />
     <News :articles="articles" />
     <Works />
-    <ContactForm :formData.sync="formData" @handleSubmit="handleSubmit" />
   </div>
 </template>
 
 <script>
 export default {
-  data() {
-    return {
-      formData: {
-        name: {
-          name: "entry.554924998",
-          data: "",
-        },
-        studentEmail: {
-          name: "entry.677875180",
-          data: "",
-        },
-        subject: {
-          name: "entry.750897921",
-          data: "",
-        },
-        content: {
-          name: "entry.1095844342",
-          data: "",
-        },
-      },
-    };
-  },
+  data() {},
   async asyncData({ $axios }) {
     const url = `/v1/databases/${process.env.NOTION_DB}/query`;
     const current_day = new Date();
@@ -69,28 +47,6 @@ export default {
       articles: articles.data.results,
     };
   },
-  methods: {
-    handleSubmit() {
-      const submitParams = new FormData();
-      Object.keys(this.formData).forEach((input) => {
-        submitParams.append(this.formData[input]["name"], this.formData[input]["data"]);
-      });
-      const CORS_PROXY = "https://cors-anywhere.herokuapp.com/";
-      const formUrl =
-        "https://docs.google.com/forms/u/0/d/e/1FAIpQLSdBXORU6jQT6e9nvOJaGd9bJQkPsHlHV8iqNc3txb48xv_xiw/formResponse";
-      this.$axios
-        .post(CORS_PROXY + formUrl, submitParams)
-        .then((res) => {
-          this.formData = "";
-          this.$nextTick(() => {
-            this.$bvModal.hide("configm");
-          });
-          alert("お問い合わせが正常に送信されました\nご連絡をお待ちください");
-        })
-        .catch((e) => {
-          alert(e);
-        });
-    },
-  },
+  methods: {},
 };
 </script>
