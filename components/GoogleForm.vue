8dc40066aadb5a261e422a61d4eb0d83fc21124f
appleRui <apple31581@gmail.com>
2022-01-03 17:47:31 +0900

    Fix/220103/GoogleForm Path DirectWrite

--- components/GoogleForm.vue
+++ components/GoogleForm.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="form-contetn">
     <iframe
-      :src="this.form_url"
+      src="https://docs.google.com/forms/d/e/1FAIpQLSfkBo0Qssm236uYBB1myDyq3zBJg0Wx-Y-mO4qkMpoex35viQ/viewform?embedded=true"
       width="640"
       height="720"
       frameborder="0"
@@ -14,10 +14,8 @@
 
 <script>
 export default {
-  data({$config}) {
-    return {
-      form_url: $config.GOOGLE_FORM + "?embedded=true",
-    };
+  data() {
+    return {};
   },
 };
 </script>
