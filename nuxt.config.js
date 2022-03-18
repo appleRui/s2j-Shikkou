2e052adbbb0be38248982666bfd8524736c4da4a
appleRui <65525612+appleRui@users.noreply.github.com>
2022-03-18 17:27:15 +0900

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -38,7 +38,7 @@ export default {
     link: [{
       rel: 'icon',
       type: 'image/x-icon',
-      href: ''https://www.dendai.ac.jp/common/images/global/favicon.ico''
+      href: 'https://www.dendai.ac.jp/common/images/global/favicon.ico'
     }]
   },
 
